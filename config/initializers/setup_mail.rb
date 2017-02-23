4daba0e98b8bb645749fff0bdd759f9e22bea516
Tara McParland <tara.mcparland@gmail.com>
2017-02-23 01:11:17 +0000

    testing2

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@
       address:        'smtp.sendgrid.net',
       port:           '2525',
       authentication: :plain,
-      user_name:      ENV['app64038828@heroku.com'],
-      password:       ENV['w1qbu2mc0734'],
+      user_name:      ENV['SENDGRID_USERNAME'],
+      password:       ENV['SENDGRID_PASSWORD'],
       domain:         'heroku.com',
       enable_starttls_auto: true
     }
